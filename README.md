a5daba2e44b952c5f296dd0b6c1e741879bc3df9
Hrvoje Kusulja <hkusulja@nodefusion.com>
2025-04-29 16:35:45 +0200

    initi2

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# NodefusionMSP.Manual
-Nodefusion IT Managed Service Provider User Manual
+# Nodefusion MSP User Manual
+
+![Aexum](static/img/aexum-logo-heading-light.svg#gh-light-mode-only)![Nodefusion MSP](static/img/aexum-logo-heading-dark.svg#gh-dark-mode-only)
+
+This is the repository for the Nodefusion IT Managed Service Provider User Manual, using docusarus.
+
+## Building and running
+
+*Build status: ![Build Status](https://github.com/Nodefusion/NodefusionMSP.Manual/actions/workflows/deploy.yml/badge.svg)*
+
+- Dev
+
+```bash
+    npm start
+```
+
+- Build
+
+```bash
+    npm build
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,145 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Nodefusion MSP User Manual',
+  tagline: 'NodefusionMSP.UserManual',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://msp-manual.nodefusion.com/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Nodefusion', // Usually your GitHub org/user name.
+  projectName: 'NodefusionMSP.UserManual', // Usually your repo name.
+
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Nodefusion/NodefusionMSP.Manual/tree/main/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ['rss', 'atom'],
+        //     xslt: true,
+        //   },
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: 'warn',
+        //   onInlineAuthors: 'warn',
+        //   onUntruncatedBlogPosts: 'warn',
+        // },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/msp-logo.png',
+      navbar: {
+        title: 'Introduction',
+        logo: {
+          alt: 'Aexum',
+          src: 'img/msp-logo-heading-light.svg',
+          srcDark: 'img/msp-logo-heading-dark.svg',
+        },
+        items: [
+          {
+            href: 'https://github.com/Nodefusion/NodefusionMSP.Manual',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      algolia: {
+        appId: 'T17L45VSEH',
+        apiKey: '8c2a6c856776945b60f394e86e124a66',
+        indexName: 'app-manual-aexum',
+        insights: true,
+      },
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Nodefusion',
+                href: 'https://www.nodefusion.com/en-us/',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/Nodefusion',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/nodefusion',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/nodefusion',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Nodefusion`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
